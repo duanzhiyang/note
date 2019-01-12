135e73950072fe530bcd6790c25fe9f133d4977f
duanzhiyang <duanzhiyang@neolix.cn>
2019-01-12 18:36:54 +0800

    分库分表demo

---
+++ demo/src/main/java/com/example/demo/entity/UserInfo.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+public class UserInfo {
+    private Long userId;
+
+    private String userName;
+
+    private String account;
+
+    private String password;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
---
+++ demo/src/main/java/com/example/demo/mapper/UserInfoMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserInfoMapper {
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Long userId);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}

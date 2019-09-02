1fecafeb44d01a60c7aab193aa3217002beccdae
duanzhiyang <duanzhiyang@neolix.cn>
2019-09-02 18:11:26 +0800

    OpenResty(Nginx+Lua)开发入门 luaApi 模块指令

---
+++ example/lua/init.lua
@@ -0,0 +1,10 @@
+local redis = require 'resty.redis'  
+local cjson = require 'cjson'  
+  
+--全局变量，不推荐  
+count = 1  
+  
+--共享全局内存  
+local shared_data = ngx.shared.shared_data  
+shared_data:set("count", 2)
+shared_data:set("newCount",100)  
--- example/lua/test.lua
+++ example/lua/test.lua
@@ -1,1 +1,7 @@
-ngx.say("小明酷炸了好吗");
+count = count + 1  
+ngx.say("global variable : ", count)  
+local shared_data = ngx.shared.shared_data  
+ngx.say(", shared memory : ", shared_data:get("count"))  
+ngx.say(", shared newCount : ", shared_data:get("newCount"))  
+shared_data:incr("count", 1)  
+ngx.say("hello world")
